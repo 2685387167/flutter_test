d5cc0e60b9a2f497863f6116a302399c6463febc
tzl <2685387167@qq.com>
2021-01-05 22:20:12 +0800

    页面实战

--- lib/main.dart
+++ lib/main.dart
@@ -1,138 +1,199 @@
-// Copyright 2018 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:flutter/foundation.dart'; //new
+import 'package:flutter/cupertino.dart'; //new
 
-void main() => runApp(MyApp());
+// Modify the FriendlychatApp class definition in main.dart.
 
-class MyApp extends StatelessWidget {
+class FriendlychatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // final wordPair = WordPair.random();
-    // return new MaterialApp(
-    //   title: 'Welcome',
-    //   home: new Scaffold(
-    //     appBar: new AppBar(
-    //       title: new Text('Welcome'),
-    //     ),
-    //     body: new Center(
-    //       // child: Text(wordPair.asPascalCase),
-    //       child: new RandomWords(),
-    //     ),
-    //   ),
-    // );
     return new MaterialApp(
-      title: 'Startup Name Generator',
-      theme: new ThemeData(
-        primaryColor: Colors.white,
-      ),
-      home: new RandomWords(),
+      title: "Friendlychat",
+      theme: defaultTargetPlatform == TargetPlatform.iOS //new
+          ? kIOSTheme //new
+          : kDefaultTheme, //new
+      home: new ChatScreen(),
     );
   }
 }
 
-class RandomWords extends StatefulWidget {
-  @override
-  createState() => new RandomWordsState();
+void main() {
+  runApp(new FriendlychatApp());
 }
 
-class RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _saved = new Set<WordPair>();
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-  // 此处实现了收藏列表页
-  void _pushSaved() {
-    Navigator.of(context).push(
-      new MaterialPageRoute(
-        builder: (context) {
-          final tiles = _saved.map(
-            (pair) {
-              return new ListTile(
-                title: new Text(
-                  pair.asPascalCase,
-                  style: _biggerFont,
-                ),
-              );
-            },
-          );
-          final divided = ListTile.divideTiles(
-            context: context,
-            tiles: tiles,
-          ).toList();
-          return new Scaffold(
-            appBar: new AppBar(
-              title: new Text('Saved Suggestions'),
-            ),
-            body: new ListView(children: divided),
-          );
-        },
-      ),
-    );
-  }
+class ChatScreen extends StatefulWidget {
+  //modified
+  @override //new
+  State createState() => new ChatScreenState(); //new
+}
 
+// Add the ChatScreenState class definition in main.dart.
+
+class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
+  //new
+  final List<ChatMessage> _messages = <ChatMessage>[];
+  final TextEditingController _textController = new TextEditingController();
+  bool _isComposing = false;
   @override
   Widget build(BuildContext context) {
-    // final wordPair = new WordPair.random();
-    // return new Text(wordPair.asPascalCase);
-    // 此处实现了无限滚动
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('data'),
-        actions: <Widget>[
-          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved),
-        ],
+        title: new Text("Friendlychat"),
+        elevation: Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
       ),
-      body: _buildSuggestions(),
+      body: new Container(
+          //modified
+          child: new Column(
+            //modified
+            children: <Widget>[
+              new Flexible(
+                child: new ListView.builder(
+                  padding: new EdgeInsets.all(8.0),
+                  reverse: true,
+                  itemBuilder: (_, int index) => _messages[index],
+                  itemCount: _messages.length,
+                ),
+              ),
+              new Divider(height: 1.0),
+              new Container(
+                decoration:
+                    new BoxDecoration(color: Theme.of(context).cardColor),
+                child: _buildTextComposer(),
+              ),
+            ],
+          ),
+          decoration: Theme.of(context).platform == TargetPlatform.iOS //new
+              ? new BoxDecoration(
+                  //new
+                  border: //new
+                      new Border(
+                          top: new BorderSide(color: Colors.grey[200]))) //new
+              : null), //new
     );
   }
 
-  Widget _buildSuggestions() {
-    return new ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        // 对于每个建议的单词对都会调用一次itemBuilder，然后将单词对添加到ListTile行中
-        // 在偶数行，该函数会为单词对添加一个ListTile row.
-        // 在奇数行，该函数会添加一个分割线widget，来分隔相邻的词对。
-        // 注意，在小屏幕上，分割线看起来可能比较吃力。
-        itemBuilder: (context, i) {
-          // 在每一列之前，添加一个1像素高的分隔线widget
-          if (i.isOdd) return new Divider();
-
-          // 语法 "i ~/ 2" 表示i除以2，但返回值是整形（向下取整），比如i为：1, 2, 3, 4, 5
-          // 时，结果为0, 1, 1, 2, 2， 这可以计算出ListView中减去分隔线后的实际单词对数量
-          final index = i ~/ 2;
-          // 如果是建议列表中最后一个单词对
-          if (index >= _suggestions.length) {
-            // ...接着再生成10个单词对，然后添加到建议列表
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-          return _buildRow(_suggestions[index]);
-        });
+  Widget _buildTextComposer() {
+    return new IconTheme(
+        data: new IconThemeData(color: Theme.of(context).accentColor),
+        child: new Container(
+          margin: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: new Row(children: <Widget>[
+            new Flexible(
+              child: new TextField(
+                controller: _textController,
+                onChanged: (String text) {
+                  //new
+                  setState(() {
+                    //new
+                    _isComposing = text.length > 0; //new
+                  }); //new
+                }, //new
+                onSubmitted: _handleSubmitted,
+                decoration:
+                    new InputDecoration.collapsed(hintText: "Send a message"),
+              ),
+            ),
+            new Container(
+                margin: new EdgeInsets.symmetric(horizontal: 4.0),
+                child: Theme.of(context).platform == TargetPlatform.iOS
+                    ? //modified
+                    new CupertinoButton(
+                        //new
+                        child: new Text("Send"), //new
+                        onPressed: _isComposing //new
+                            ? () => _handleSubmitted(_textController.text) //new
+                            : null,
+                      )
+                    : //new
+                    new IconButton(
+                        //modified
+                        icon: new Icon(Icons.send),
+                        onPressed: _isComposing
+                            ? () => _handleSubmitted(_textController.text)
+                            : null,
+                      )),
+          ]),
+        ));
   }
 
-  Widget _buildRow(WordPair pair) {
-    final alreadySaved = _saved.contains(pair);
-    return new ListTile(
-      title: new Text(
-        pair.asPascalCase,
-        style: _biggerFont,
+  void _handleSubmitted(String text) {
+    _textController.clear();
+    setState(() {
+      //new
+      _isComposing = false; //new
+    }); //new
+    ChatMessage message = new ChatMessage(
+      text: text,
+      animationController: new AnimationController(
+        duration: new Duration(milliseconds: 700),
+        vsync: this,
       ),
-      // 此处实现心形图案，不带点击功能
-      trailing: new Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      // 此处使得心形图案变得可点击
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
-      },
     );
+    setState(() {
+      _messages.insert(0, message);
+    });
+    message.animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    //new
+    for (ChatMessage message in _messages) //new
+      message.animationController.dispose(); //new
+    super.dispose(); //new
   }
 }
+
+class ChatMessage extends StatelessWidget {
+  ChatMessage({this.text, this.animationController});
+  final String text;
+  final AnimationController animationController;
+  @override
+  Widget build(BuildContext context) {
+    return new SizeTransition(
+        //new
+        sizeFactor: new CurvedAnimation(
+            //new
+            parent: animationController, //new
+            curve: Curves.easeOut //new
+            ), //new
+        axisAlignment: 0.0, //new
+        child: new Container(
+          //modified
+          margin: const EdgeInsets.symmetric(vertical: 10.0),
+          child: new Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              new Container(
+                margin: const EdgeInsets.only(right: 16.0),
+                child: new CircleAvatar(child: new Text(_name[0])),
+              ),
+              new Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  new Text(_name, style: Theme.of(context).textTheme.subhead),
+                  new Container(
+                    margin: const EdgeInsets.only(top: 5.0),
+                    child: new Text(text),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ) //new
+        );
+  }
+}
+
+const String _name = "Your Name";
+
+final ThemeData kIOSTheme = new ThemeData(
+  primarySwatch: Colors.orange,
+  primaryColor: Colors.grey[100],
+  primaryColorBrightness: Brightness.light,
+);
+
+final ThemeData kDefaultTheme = new ThemeData(
+  primarySwatch: Colors.purple,
+  accentColor: Colors.orangeAccent[400],
+);
