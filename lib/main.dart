70ea996843ac14b0e6571a7950aa4c377f99fc1f
tzl <2685387167@qq.com>
2021-01-06 17:20:20 +0800

    加了点注释

--- lib/main.dart
+++ lib/main.dart
@@ -116,6 +116,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
+// 状态管理
 class FavoriteWidget extends StatefulWidget {
   @override
   _FavoriteWidgetState createState() => _FavoriteWidgetState();
